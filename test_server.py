f4f0e1b31146421ad82b10926b01d85235d4f86c
infra <mukil@langchain.dev>
2024-06-04 13:47:49 -0700

    docs: add README for LangSmith model server

--- test_server.py
+++ test_server.py
@@ -2,7 +2,7 @@ from langserve import RemoteRunnable
 
 chat_model = RemoteRunnable("http://localhost:8080/chat")
 
-print(chat_model.invoke("help"))
+print(f"Pinging chat model: {chat_model.invoke('help').content}")
 
 instruct_model = RemoteRunnable("http://localhost:8080")
-print(instruct_model.invoke("help")
+print(f"Pinging instruct model: {instruct_model.invoke('help')}")
